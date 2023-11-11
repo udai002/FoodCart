b113dedf9bad81d134b0c0e7992ce79c0bb1de1a
udai sai <karmuriudaisai002@gmail.com>
2023-11-11 11:51:57 +0530

    added paragraph

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className='font-bold text-3xl'>FoodCart</h1>
+      <p>hello chukka how is this</p>
       
     </main>
   )
